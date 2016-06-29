2de716192139b136d489eff07dc337f88d619fba
kenaxle <kenaxle@hotmail.com>
2016-06-29 23:17:49 +0900

    testing

--- src/kr/ac/hanyang/platform/oCampPlatformLauncher.java
+++ src/kr/ac/hanyang/platform/oCampPlatformLauncher.java
@@ -3,30 +3,40 @@ package kr.ac.hanyang.platform;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.brooklyn.api.mgmt.ManagementContext;
+import org.apache.brooklyn.camp.AggregatingCampPlatform;
+import org.apache.brooklyn.camp.CampPlatform;
+import org.apache.brooklyn.camp.brooklyn.BrooklynCampPlatform;
 import org.apache.brooklyn.camp.brooklyn.BrooklynCampPlatformLauncherAbstract;
+import org.apache.brooklyn.camp.server.rest.CampServer;
+import org.apache.brooklyn.camp.spi.PlatformRootSummary;
+import org.apache.brooklyn.core.mgmt.internal.LocalManagementContext;
 import org.apache.brooklyn.launcher.BrooklynLauncher;
 import org.apache.brooklyn.launcher.camp.BrooklynCampPlatformLauncher;
 
-public class AppPlatformLauncher extends BrooklynCampPlatformLauncher {
-	BrooklynLauncher launcher;
-	BrooklynCampPlatformLauncherAbstract yLauncher;
+public class oCampPlatformLauncher /*extends BrooklynCampPlatformLauncher*/ {
+
 	
+	private oCampPlatform platform;
+	private ManagementContext mgmt;
 	
-	public BrooklynCampPlatformLauncher launch(){
-		launcher = BrooklynLauncher.newInstance().start();
-		//yLauncher	
+	public oCampPlatformLauncher launch() {
+		if (platform != null)
+			//throw new Exception("platform already exists!"); // will have to fix this
+			System.out.println("There is an exception here. the platform already exists");
+		if (mgmt == null)
+			mgmt = new LocalManagementContext();
+		platform = new oCampPlatform(PlatformRootSummary.builder().name("Brooklyn CAMP Platform").build(),
+                getManagement());
 		return this;
 	}
 	
-	public static void main (String[] args){
-		new AppPlatformLauncher().launch();
+	public oCampPlatform getCampPlatform(){
+		return platform;
 	}
 	
-	public void stopServers() throws Exception {
-        yLauncher = new SimpleYamlLauncher().newPlatformLauncher();
-		//launcher.getServerDetails().getWebServer().stop();
-        //campServer.stop();
-       // yLauncher.launch();
-    }
+	ManagementContext getManagement(){
+		return mgmt;
+	}
 	
 }
